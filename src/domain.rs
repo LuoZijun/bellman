d55208f5da50240edb5269b92f6875e01489fb39
Luozijun <luozijun.assistant@gmail.com>
2019-11-22 15:43:54 +0800

    .

--- src/domain.rs
+++ src/domain.rs
@@ -298,8 +298,8 @@ fn best_fft<E: Engine, T: Group<E>>(
         parallel_fft(&mut cpu_res, worker, omega, log_n, log_cpus);
 
         debug!("GPU Result Len: {:?}  CPU Result Len: {:?}", a.len(), cpu_res.len());
-        debug!("GPU RES == PCPU = {:?}", a == cpu_res);
-        
+        debug!("GPU RES == PCPU = {:?}", a == &mut cpu_res);
+
     } else {
         let log_cpus = worker.log_num_cpus();
         if log_n <= log_cpus {
